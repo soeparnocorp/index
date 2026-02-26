35723dbf9b6361b126212134af3235e3014e28d3
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-02-27 04:58:01 +0700

    consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'READTalk Messenger';
+export const SITE_TITLE = 'READTalk';
 export const SITE_DESCRIPTION = 'Welcome to READTalk Inc.';
